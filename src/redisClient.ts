4d0e39fc0cd06464afbca379a7a8c53aeb014eab
Doublo <83957749+Doublo54@users.noreply.github.com>
2025-12-31 16:07:28 +0100

    feat: add redis url

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 redis.on("error", function (err) {
   console.log("Could not establish a connection with redis. ", err);
 });
